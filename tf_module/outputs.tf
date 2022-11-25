4a0b75ec61afcfe5bb679cc9e6b48c1815cf97a4
EC2 Default User <ec2-user@ip-10-0-0-31.ec2.internal>
2022-11-25 19:30:22 +0000

    added outputs

--- tf_module/outputs.tf
+++ tf_module/outputs.tf
@@ -13,27 +13,31 @@
 #   value = "ssh -i ${var.worker_key_name}.pem ubuntu@${aws_eip.localzones-ip["las"].public_ip}"
 # }
 
-output "bastion_ssh" {
+output "BASTION_SSH" {
   description = "Command to SSH into Bastion Host Instance"
   value = "ssh -i ${var.worker_key_name}.pem -A ec2-user@${aws_instance.bastion_host_instance.public_ip}"
 }
 
-output "wavelength_ssh" {
+output "WAVELENGTH_SSH" {
   description = "Command to SSH into Wavelength Zone Instance"
   value = "ssh ubuntu@${aws_instance.webrtc_instance["las"].private_ip}"
 }
 
-output "vpc_id" {
+output "VPC_ID" {
   description = "ID of your Virtual Private Cloud (VPC)"
   value= aws_vpc.hyb303_vpc.id
 }
-output "region_subnet_1_id" {
+output "REGION_AZ1_SUBNET" {
   description = "ID of your 1st parent region subnet"
   value = aws_subnet.region_subnets["az1"].id
 }
-output "region_subnet_2_id" {
+output "REGION_AZ2_SUBNET" {
   description = "ID of your 2nd parent region subnet"
   value = aws_subnet.region_subnets["az2"].id
+}
+output "LAS_WLZ_SUBNET" {
+  description = "ID of your Las Vegas Wavelength Zone subnet"
+  value = aws_subnet.wavelength_subnets["las"].id
 }
 
 
